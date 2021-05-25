36b66659476ab57cb5475e5b5fc26a9b7091f840
Gota <guotianxiang@chachazhan.com>
2021-05-25 13:48:16 +0800

    Change method signature to pointer

--- roundtripper.go
+++ roundtripper.go
@@ -11,7 +11,7 @@ type RoundTripper struct {
 	Parent http.RoundTripper
 }
 
-func (e RoundTripper) RoundTrip(req *http.Request) (resp *http.Response, err error) {
+func (e *RoundTripper) RoundTrip(req *http.Request) (resp *http.Response, err error) {
 	if resp, err = e.next(req); err != nil {
 		err = e.onNetworkError(req, err)
 		return
@@ -26,19 +26,19 @@ func (e RoundTripper) RoundTrip(req *http.Request) (resp *http.Response, err err
 	return
 }
 
-func (e RoundTripper) next(req *http.Request) (resp *http.Response, err error) {
-	rt := http.DefaultClient.Transport
+func (e *RoundTripper) next(req *http.Request) (resp *http.Response, err error) {
+	rt := http.DefaultTransport
 	if e.Parent != nil {
 		rt = e.Parent
 	}
 	return rt.RoundTrip(req)
 }
 
-func (e RoundTripper) onNetworkError(req *http.Request, err error) error {
+func (e *RoundTripper) onNetworkError(req *http.Request, err error) error {
 	return Annotate(err, WithCode(Internal), WithMessage(req.URL.String()))
 }
 
-func (e RoundTripper) onBusinessError(req *http.Request, resp *http.Response, err error) error {
+func (e *RoundTripper) onBusinessError(req *http.Request, resp *http.Response, err error) error {
 	defer func() { _ = resp.Body.Close() }()
 
 	if e.isJson(resp) {
@@ -55,11 +55,11 @@ func (e RoundTripper) onBusinessError(req *http.Request, resp *http.Response, er
 		WithHttpCode(resp.StatusCode))
 }
 
-func (e RoundTripper) isSuccess(resp *http.Response) bool {
+func (e *RoundTripper) isSuccess(resp *http.Response) bool {
 	return resp.StatusCode >= http.StatusOK && resp.StatusCode < http.StatusBadRequest
 }
 
-func (e RoundTripper) isJson(resp *http.Response) bool {
+func (e *RoundTripper) isJson(resp *http.Response) bool {
 	contentType := resp.Header.Get("Content-Type")
 	return strings.HasPrefix(contentType, "application/json")
 }
